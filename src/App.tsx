56891c17a72d8a88bda483f8ae3c7f833148d86b
DinisFigueiras <106346721+DinisFigueiras@users.noreply.github.com>
2025-08-27 18:28:52 +0100

    fix analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Analytics />
     <TooltipProvider>
       <Toaster />
       <Sonner />
@@ -22,6 +21,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics/>
     </TooltipProvider>
   </QueryClientProvider>
 );
